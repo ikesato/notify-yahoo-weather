17f551bc02073e644b5f8364c5cfe083d1dbc0b3
ikeda <ikeda@koozyt.com>
2014-12-22 02:44:11 +0900

    Implement notifications

--- app.rb
+++ app.rb
@@ -9,12 +9,16 @@ weather = YolWeather.new(appid: "dj0zaiZpPTZpTUZRUzI3MmN4WiZzPWNvbnN1bWVyc2VjcmV
 weather.location = {lat: 35.649657, lng: 139.752162}
 weather.sync
 
+notifier = Slack::Notifier.new "https://hooks.slack.com/services/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"
+notifier.ping "Bot started"
+
 counter = 1
 EM::defer do
   loop do
     sleep 10*60
     counter += 1
     weather.sync
+    notifier.ping notification_messages
   end
 end
 
@@ -28,18 +32,14 @@ end
 
 get '/debug/weather' do
   content_type 'text/plain'
-  PP.pp(weather.last_weather, '')
+  PP.pp(weather.weather, '')
+end
+
+get '/debug/notifications' do
+  content_type 'text/plain'
+  PP.pp(weather.notifications, '')
 end
 
 get '/debug/counter' do
   counter.to_s
 end
-
-
-#notifier = Slack::Notifier.new "https://hooks.slack.com/services/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"
-#notifier.ping "Hello World 101
-
-#location = {lat: 35.649657, lng: 139.752162}
-#appid = "dj0zaiZpPTZpTUZRUzI3MmN4WiZzPWNvbnN1bWVyc2VjcmV0Jng9ZDI"
-#
-#pp a
--- yolp-weather.rb
+++ yolp-weather.rb
@@ -8,9 +8,11 @@ require 'pp'
 class YolWeather
   attr_accessor :location
   attr_accessor :appid
+  attr_reader :weather, :notifications
 
   def initialize(appid: nil, location: nil)
     @appid = appid
+    @notifications = []
   end
 
   def sync
@@ -35,11 +37,13 @@ class YolWeather
     end
   end
 
-  def notifications
-  end
-
-  def last_weather
-    @weather
+  def notification_messages
+    messages = []
+    @notifications.each do |n|
+      next if n[:sended]
+      messages << make_notification_message(n)
+    end
+    messages
   end
 
 
@@ -59,35 +63,99 @@ class YolWeather
   # 地点は見ない
   # FeatureWeatherList だけ参照
   def make_notifications
-    # Detect edge weather
+    # Detect weather edges
     edges = []
     futures = @weather["Feature"].first["Property"]["WeatherList"]["Weather"]
     futures.each do |f|
+      next if f["Type"] != "forecast"
       status = {time: Time.zone.parse(f["Date"]), fine: f["Rainfall"].to_f == 0.0}
-      @last_observation = status if f["Type"] == "observation"
-      el = edges.last
-      edges << status if el.nil? || el[:fine] != status[:fine]
+      edges << status if edges.last.nil? || edges.last[:fine] != status[:fine]
     end
+    pp ["aaaaaaaaa 0", futures]
 
-    # Resolv status and duration
+    # Resolve status and duration and key
     edges.each_with_index do |e, i|
-      if i == 0
-        e[:status] = :fine
-      elsif i == edges.length-1
-        e[:status] = :rain
+      if i == 0 || i == edges.length-1
+        e[:status] = e[:fine] ? :fine : :rain
       else
-        if e[:fine]
-          e[:status] = :fine_once
-        else
-          e[:status] = :rain_once
-        end
+        e[:status] = e[:fine] ? :fine_once : :rain_once
         e[:duration] = edges[i+1][:time] - e[:time]
       end
+      e[:key] = e[:time].to_i.to_s + ":" + e[:status].to_s + ":" + e[:duration].to_s
     end
+    pp ["aaaaaaaaa 1", edges]
 
-    # Merge last_notifications
-    ns = []
-    @last_notifications
+    # Merge notifications
+    ln = @notifications.last
+    if ln && edges.first[:status] != ln[:status]
+      edges.shift
+    end
+    edges.each do |e|
+      next if @notifications.any? do |n|
+        n[:key] == e[:key]
+      end
+      @notifications << e
+    end
+    pp ["aaaaaaaaa 2", edges]
+    pp ["aaaaaaaaa 2.2", @notifications]
+
+    # Sort
+    @notifications.sort do |a,b|
+      if a[:time] == b[:time]
+        status_to_i(a[:status]) <=> status_to_i(b[:status]) 
+      else
+        a[:time] <=> b[:time]
+      end
+    end
+    pp ["aaaaaaaaa 3", @notifications]
+
+    # Uniq
+    ln = nil
+    @notifications.delete_if do |n|
+      if ln && ln[:time] == n[:time]
+        true
+      else
+        ln = n
+        false
+      end
+    end
+    pp ["aaaaaaaaa 4", @notifications]
+
+    # Remove old notifications
+    ln = @notifications.last
+    @notifications.delete_if do |n|
+      n[:time] < Time.zone.now
+    end
+    pp ["aaaaaaaaa 5", @notifications]
+
+    # 最後の一つは必ず残す
+    @notifications << ln if @notifications.empty?
+    pp ["aaaaaaaaa 6", @notifications]
   end
 
+  def status_to_i(status)
+    case status
+    when :fine
+      2
+    when :rain
+      1
+    when :fine_once
+      4
+    when :rain_once
+      3
+    end
+  end
+
+  def make_notification_message(n)
+    case n[:status]
+    when :fine
+      n[:time].strftime("%H時%M分に晴れます。")
+    when :rain
+      n[:time].strftime("%H時%M分に雨が降ります。")
+    when :fine_once
+      "#{n[:time].strftime("%H時%M分")}から#{n[:duration]/60}分間だけ晴れます。"
+    when :rain_once
+      "#{n[:time].strftime("%H時%M分")}から#{n[:duration]/60}分間だけ雨が降ります。"
+    end
+  end
 end
